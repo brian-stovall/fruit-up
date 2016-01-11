fbbac01955da5e8977a4c0c6eae05b0c95cfa653
Brian Stovall <hoppenmaus@gmail.com>
2016-01-10 21:01:57 -0500

    animation code working! need to tie speeds/term. velo. etc to screen %s rather that px

--- fruity.js
+++ fruity.js
@@ -15,6 +15,7 @@ document.addEventListener('DOMContentLoaded', function() {
 	const sheetSize = {'width':128, 'height':96}
 	//the scaling factor for the fruits: viewwidth*desired percentage/spriteWidth
 	const spriteScale = viewport.offsetWidth * .10 / spriteWidth; 
+	const TERMINAL_VELOCITY = 300; 
 
 	function skinSprite(skinName) {
 		if (!this) { console.log('this error - switchSprite'); return;}
@@ -40,16 +41,17 @@ document.addEventListener('DOMContentLoaded', function() {
 		this.parent.appendChild(this.element);
 	}
 	// causes a sprite to move according to it's dx and dy
-	function update(gravity, drag) {
+	function update(gravity, drag, dt) {
 		if (!this) { console.log('this error - update'); return;}
 		//console.log('update called');
-		var dt = viewport.getDt();
 		if (gravity || this.dx || this.dy ) {
-			drag = drag * (this.dt > 0) ? 1 : -1;
-			this.dx += drag; 
+			//drag = drag * (this.dt > 0) ? 1 : -1;
+			//this.dx += drag; 
+			this.dy += gravity;
+			if (this.dy > TERMINAL_VELOCITY) this.dy = TERMINAL_VELOCITY;
 			this.xPos += this.dx * dt;
-			this.yPos += (this.dy + gravity) * dt;
-			//console.log(this.yPos);
+			this.yPos += this.dy * dt;
+			//console.log(this.yPos, this.dy, gravity);
 		}
 	}
 
@@ -83,14 +85,15 @@ document.addEventListener('DOMContentLoaded', function() {
 	}
 	
 	function animate(gravity, drag) {
+		var dt = viewport.getDt();
 		var dieList = [];
 		if (sprites.length){
 			//console.log(sprites.length + ' sprites.length');
-			window.requestAnimationFrame(animate);
+			window.requestAnimationFrame(function() {animate(gravity, drag)});
 			for (var i = 0; i < sprites.length; i++) {
-				sprites[i].update(gravity, drag);
+				sprites[i].update(gravity, drag, dt);
 				sprites[i].blit();
-				if (sprites[i].yPos >= viewport.offsetHeight){
+				if (sprites[i].yPos + spriteWidth * spriteScale >= viewport.offsetHeight){
 					//console.log(sprites[i].name + ' offscreen!');
 					sprites[i].destroy();
 					dieList.push(i);
@@ -104,11 +107,17 @@ document.addEventListener('DOMContentLoaded', function() {
 	function test() {
 		//this is where the blender is
 		var blenderMouth = [.48 * viewport.offsetWidth, .55 * viewport.offsetHeight];
-		var gravity = .1; //gravity in pixels/s - FIX this should be a %screen /s and get scaled!
-		var drag = .1; //left-right drag in pixels/s - FIX this should be a %screen /s and get scaled!
-		//sprites.push(new Sprite('cherry', blenderMouth[0], blenderMouth[1]));
-		sprites.push(new Sprite('pear', 380, 0));
-		sprites[0].dx = 2;
+		var gravity = 25; //additive gravity in pixels/s - FIX should have a 'terminal velocity'?
+		var drag = 0; //left-right drag in pixels/s - FIX this should be a %screen /s and get scaled!
+		sprites.push(new Sprite('cherry', blenderMouth[0], blenderMouth[1]));
+		sprites.push(new Sprite('pear', blenderMouth[0], blenderMouth[1]));
+		sprites.push(new Sprite('pineapple', blenderMouth[0], blenderMouth[1]));
+		sprites[0].dx = 200;
+		sprites[0].dy = -720;
+		sprites[1].dx = -250;
+		sprites[1].dy = -620;
+		sprites[2].dx = -450;
+		sprites[2].dy = -520;
 		//start our timer
 		viewport.getDt = calcDt();
 		animate(gravity, drag);
